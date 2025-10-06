a27ef20bd8ebd06339fbebd3ebbb5ef91eb1ef3c
Shefin <shefinptr@gmail.com>
2025-10-06 13:57:13 +0530

    first commit

---
+++ basicMaths.js
@@ -0,0 +1,13 @@
+//Count digits
+
+function countDigits(n){
+    let count = 0;
+    while(n > 0){
+        count = count + 1
+        n = Math.floor(n / 10)
+    }
+
+    return count;
+}
+
+console.log(countDigits(1234));
---
+++ patterns.js
@@ -0,0 +1,242 @@
+
+// * * * * 
+// * * * * 
+// * * * * 
+// * * * * 
+// for(let i = 0; i < 4; i++){
+//     let row = '';
+//     for(let j = 0; j < 4;j++){
+//         row += '* '
+//     }
+//     console.log(row);
+    
+// }
+
+
+// let pattern = ''
+// for(let i = 0; i < 4; i++){
+//     for(let j = 0; j < 4; j++){
+//         pattern+= '* ';
+//     }
+
+//     pattern += '\n'
+// }
+
+// console.log(pattern);
+
+
+
+
+// * 
+// * * 
+// * * * 
+// * * * * 
+// for(let i = 0 ; i < 4; i ++){
+//     let row = ''
+//     for(let j = 0 ; j <= i; j++){
+//         row += '* '
+//     }
+//     console.log(row);
+    
+// }
+
+
+
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+
+// let n = 4
+// let row = '';
+// for(let i = 1; i <= n; i++){
+//     for(let j = 1; j <= i; j++){
+//         row += j+' '
+//     }
+//     row += '\n'
+// }
+// console.log(row);
+
+
+
+
+
+// 1 
+// 2 2 
+// 3 3 3 
+// 4 4 4 4
+
+// let n = 4
+// let row = '';
+// for(let i = 1; i <= n; i++){
+//     for(let j = 1; j <= i; j++){
+//         row += i+' '
+//     }
+//     row += '\n'
+// }
+// console.log(row);
+
+
+
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
+
+// let n = 5
+// for(let i = 1; i <= n; i++){
+//     let row = ''
+//     for(let j = 0; j < n-i+1; j++){
+//         row += '* '
+//     }
+//     console.log(row);
+    
+// }
+
+
+
+
+
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
+// let n = 5
+// for(let i = 1; i <= n; i++){
+//     let row = '';
+//     for(let j = 1; j <= n-i+1; j++){
+//         row += j + ' '
+//     }
+//     console.log(row);
+    
+// }
+
+
+
+
+
+//     *    
+//    ***   
+//   *****  
+//  ******* 
+// *********
+// let n = 5
+// for(let i = 0; i < n; i++){
+//     let row = ''
+//     for(let j = 0; j < n-i-1; j++){
+//         row+= ' '
+//     }
+
+//     for(let j = 0; j < 2 *i+1; j++){
+//         row+= '*'
+//     }
+
+//     for(let j = 0; j < n-i-1; j++){
+//         row+= ' '
+//     }
+//     console.log(row);
+    
+// }
+
+
+// *************
+//  *********** 
+//   *********  
+//    *******   
+//     *****    
+//      ***     
+//       * 
+// let n = 7
+// for(let i = 0; i < n; i++){
+//     let row = ''
+//     for(let j = 0; j <i; j++){
+//         row+= ' '
+//     }
+
+//     for(let j = 0; j < 2 * n - (2*i + 1); j++){
+//         row += '*'
+//     }
+
+//     for(let j = 0; j <i; j++){
+//         row+= ' '
+//     }
+
+//     console.log(row);
+    
+// }
+
+
+
+   
+
+
+//       *      
+//      ***     
+//     *****    
+//    *******   
+//   *********  
+//  *********** 
+// *************
+// *************
+//  *********** 
+//   *********  
+//    *******   
+//     *****    
+//      ***     
+//       * 
+     
+// let n = 7
+// for(let i = 0; i < n; i++){
+//     let row = ''
+//     for(let j = 0; j < n-i-1; j++){
+//         row+= ' '
+//     }
+
+//     for(let j = 0; j < 2 *i+1; j++){
+//         row+= '*'
+//     }
+
+//     for(let j = 0; j < n-i-1; j++){
+//         row+= ' '
+//     }
+//     console.log(row);
+    
+// }
+
+
+// for(let i = 0; i < n; i++){
+//     let row = ''
+//     for(let j = 0; j <i; j++){
+//         row+= ' '
+//     }
+
+//     for(let j = 0; j < 2 * n - (2*i + 1); j++){
+//         row += '*'
+//     }
+
+//     for(let j = 0; j <i; j++){
+//         row+= ' '
+//     }
+
+//     console.log(row);
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
