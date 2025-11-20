c064111c4c9ea15a4f2079ce666cad3f1f2c4f37
shefin <shefinptr@gmail.com>
2025-11-20 14:19:49 +0530

    day one of neetcode250

---
+++ neetcode250.js
@@ -0,0 +1,97 @@
+// 242. valid anagram 
+
+// function isAnagram(s, t){
+//     if(s.length !== t.length) return false;
+
+//     let map = {};
+
+//     for(let i = 0; i < s.length; i++){
+//         let letter = s[i];
+
+//         if(!map[letter]){
+//             map[letter] = 1
+//         }else{
+//             map[letter]++
+//         }
+//     }
+
+//     for(let i = 0; i < t.length; i++){
+//         let letter = t[i];
+
+//         if(map[letter] === undefined) return false;
+//         if(map[letter] < 1) return false;
+//         map[letter]--;
+//     }
+//     return true;
+// }
+
+
+// console.log(isAnagram("anagram", "nagaram"));
+// console.log(isAnagram("anagram", "nagbram"));
+
+
+//another easy method
+
+
+// function isAnagram(s, t){
+//     if(s.length !== t.length) return false;
+
+//     let map = {};
+
+//     for(let ch of s){
+//         map[ch] = (map[ch] || 0) + 1;
+//     }
+
+//     for(let ch of t){
+//        if(!map[ch]) return false
+//        map[ch]-- 
+//     }
+//     return true
+// }
+
+// console.log(isAnagram("anagram", "nagaram"));
+// console.log(isAnagram("anagram", "nagbram"));
+
+
+
+
+//1. Two sum
+
+
+//brute force approach
+// time complexity O(n^2)
+
+
+// function twoSum(nums, target){
+
+//     for(let i = 0; i < nums.length; i++){
+//         for(let j = 0; j < nums.length; j++){
+//             if(nums[i] + nums[j] === target){
+//                 return [i, j]
+//             }
+//         }
+//     }
+
+// }
+
+// console.log(twoSum([2,7,11,15], 9));
+
+
+//Optimal way using map
+
+
+function twoSum(nums, target){
+    let map = new Map();
+    
+    for(let i = 0; i < nums.length; i++){
+        let num = target - nums[i]
+        if(map.has(num)){
+            return [i, map.get(num)]
+        } else {
+            map.set(nums[i], i)
+        }
+    }
+
+}
+
+console.log(twoSum([2,7,11,15], 9));
