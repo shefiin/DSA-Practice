dbf53fcacb4ae9639c16cf1f1a40d847b2feee24
shefin <shefinptr@gmail.com>
2025-12-12 14:06:31 +0530

    striver sde sheet

---
+++ striverAtoZ.js
@@ -0,0 +1,70 @@
+//1. Print Name N times using Recursion
+
+// function printNameNtimes(n){
+//     if(n === 0) return;
+//     let name = "shefin"
+
+//     printNameNtimes(n - 1)
+//     console.log(name)
+// }
+
+// printNameNtimes(3)
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//.2
+
+// Given an integer n, write a function to print all numbers from 1 to n (inclusive) using recursion.
+
+// You must not use any loops such as for, while, or do-while.
+// The function should print each number on a separate line, in increasing order from 1 to n.
+
+
+
+// function print1toN(n){
+//     if(n === 0) return n;
+
+//     print1toN(n - 1)
+//     console.log(n);
+    
+// }
+
+// print1toN(5)
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//3. Print N to 1 using Recursion
+
+
+// function printNto1(n){
+//     if(n === 0) return;
+
+//     console.log(n);
+//     printNto1(n - 1)
+ 
+// }
+
+// printNto1(5)
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//4. Sum of first N Natural Numbers
+
+
+// function sumOfN(n){
+//     if(n === 0) return 0;
+
+//     return n + sumOfN(n - 1);
+
+// }
+
+// console.log(sumOfN(5));
+
+
+
