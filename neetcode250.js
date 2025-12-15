2fc26234939783e186732fdc1ba0b5dfb9c024dd
shefin <shefinptr@gmail.com>
2025-12-15 12:34:12 +0530

    some more leetcode

--- neetcode250.js
+++ neetcode250.js
@@ -114,5 +114,47 @@
 
 
 
+//leetcode 1768
+
+// function mergeStrings(word1, word2){
+//     let i = 0
+//     let j = 0
+//     let result = "";
+
+//     while(i < word1.length || j < word2.length){
+//         if(i < word1.length){
+//             result += word1[i];
+//             i++;
+//         }
+
+//         if(j < word2.length){
+//             result += word2[j];
+//             j++;
+//         }
+//     }
+
+//     return result;
+// }
+
+// console.log(mergeStrings("hello", "shefin"));
+
+
+//another way
+
+
+// function mergeStrings(word1, word2){
+//     let result = "";
+//     let maxLen = Math.max(word1.length, word2.length)
+
+//     for(let i = 0; i < maxLen; i++){
+//         if(i < word1.length) result += word1[i]
+//         if(i < word1.length) result += word2[i]
+//     }
+//     return result
+// }
+// console.log(mergeStrings("hello", "shefin"));
+
+
+
 
 
